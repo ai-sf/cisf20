58ee115bc24acdd2f70349df8d8b566deb4d6cc2
Giovanni Stagnitto <nitto@nitto-laptop>
2020-03-05 19:16:25 +0100

    updating version of materializecss + deleting unnecessary files

--- js/home.js
+++ js/home.js
@@ -1,13 +1,14 @@
 (function($){
     $(function(){
-	$("#index-page").css("padding-top", $(".slider").height()*0.8);
+        //$("#index-page").css("padding-top", $(".slider").height()+50);
+	$("#index-page").css("padding-top", $(".slider").height()*1.0);
         $("#chevron-banner").click(function(){
             $('html, body').animate({
                 scrollTop: ($(".slider").height())
             },500);
         });
 	$( window ).resize(function() {
-            $("#index-page").css("padding-top", $(".slider").height()*0.8);
+            $("#index-page").css("padding-top", $(".slider").height()*1.0);
 	});
     }); // end of document ready
 })(jQuery); // end of jQuery name space
